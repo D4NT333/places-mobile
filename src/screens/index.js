b6b21b530877cd32931b8b2b9b851a879538ae5a
azterfree <asterworl@gmail.com>
2025-12-24 19:34:26 -0600

    Creando mas carpetas (aunque estan en ingles me gusta) para que quede bonito

---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+export { default as HomeScreen } from "./HomeScreen";
+export { default as SearchScreen } from "./SearchScreen";
+export { default as AddPlacesScreen } from "./AddPlacesScreen";
+export { default as MetricsScreen } from "./MetricsScreen";
+export { default as ProfileScreen } from "./ProfileScreen";
