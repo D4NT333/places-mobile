84321bf7402f5e3aa592a38a8ba9503b474b9951
azterfree <asterworl@gmail.com>
2026-04-23 19:49:12 -0600

    Implementando registrar placeSubmission en firestore y storage, solo falta mejorar la UI de AddPlacesScreen y meter los horarios xd

--- src/services/api/createPlaceSubmission.service.js
+++ src/services/api/createPlaceSubmission.service.js
@@ -1,8 +1,24 @@
 import client from "./client";
+import { auth } from "../firebase/config";
 
 export default async function createPlaceSubmissionService(payload) {
   try {
-    const response = await client.post("/api/place-submissions", payload);
+    const idToken = await auth.currentUser?.getIdToken();
+
+    if (!idToken) {
+      throw new Error("No se pudo obtener el token del usuario.");
+    }
+
+    const response = await client.post(
+      "/api/place-submissions",
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+    );
+
     return response.data;
   } catch (error) {
     console.error(
